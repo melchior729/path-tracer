5d2ef1ca33566eaffa0f7542f75e4365973d53b7
Abhay Manoj <sanicthecheese@gmail.com>
2026-06-23 18:25:24 -0400

    feat: added frame buffer to appstate struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include <SDL3/SDL_pixels.h>
 #define SDL_MAIN_USE_CALLBACKS 1
 
 #include "SDL3/SDL_init.h"
-#include "SDL3/SDL_main.h"
 #include "SDL3/SDL_render.h"
 #include "config.hpp"
+#include "frame_buffer.hpp"
+#include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_video.h>
 #include <memory>
 
@@ -18,6 +18,7 @@ struct AppState {
   std::unique_ptr<SDL_Window, SDL_Deleter> window{};
   std::unique_ptr<SDL_Renderer, SDL_Deleter> renderer{};
   std::unique_ptr<SDL_Texture, SDL_Deleter> texture{};
+  std::unique_ptr<FrameBuffer> buffer{};
 };
 
 SDL_AppResult SDL_AppInit(void **appstate, [[maybe_unused]] int argc,
@@ -42,14 +43,13 @@ SDL_AppResult SDL_AppInit(void **appstate, [[maybe_unused]] int argc,
   state->window.reset(w);
   state->renderer.reset(r);
   state->texture.reset(t);
+  state->buffer = std::make_unique<FrameBuffer>();
 
   *appstate = state.release();
-
   return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent([[maybe_unused]] void *appstate,
-                           [[maybe_unused]] SDL_Event *event) {
+SDL_AppResult SDL_AppEvent([[maybe_unused]] void *appstate, SDL_Event *event) {
   if (event->type == SDL_EVENT_QUIT) {
     return SDL_APP_SUCCESS;
   }
@@ -57,11 +57,18 @@ SDL_AppResult SDL_AppEvent([[maybe_unused]] void *appstate,
   return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate([[maybe_unused]] void *appstate) {
+SDL_AppResult SDL_AppIterate(void *appstate) {
   auto state{static_cast<AppState *>(appstate)};
+  state->buffer->clear();
+
   SDL_RenderPresent(state->renderer.get());
   return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit([[maybe_unused]] void *appstate,
-                 [[maybe_unused]] SDL_AppResult result) {}
+void SDL_AppQuit(void *appstate, [[maybe_unused]] SDL_AppResult result) {
+  if (appstate != nullptr) {
+    std::unique_ptr<AppState> state{static_cast<AppState *>(appstate)};
+  }
+
+  SDL_Quit();
+}
