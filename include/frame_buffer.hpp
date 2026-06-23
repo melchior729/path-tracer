0bdd2abe5fead0d54817bd9713d0ef309e253dd1
Abhay Manoj <sanicthecheese@gmail.com>
2026-06-23 18:18:25 -0400

    feat: added framebuffer struct

---
+++ include/frame_buffer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "color.hpp"
+#include "config.hpp"
+#include <array>
+
+struct FrameBuffer {
+  std::array<Color, WIDTH * HEIGHT> pixels{};
+
+  constexpr FrameBuffer() { clear(); }
+
+  constexpr void set(std::size_t i, Color color) {
+    if (!in_bounds(i)) {
+      return;
+    }
+
+    pixels[i] = color;
+  }
+
+  constexpr void set(std::size_t x, std::size_t y, Color color) {
+    if (x >= WIDTH || y >= HEIGHT) {
+      return;
+    }
+
+    set(y * WIDTH + x, color);
+  }
+
+  constexpr void clear() { pixels.fill(BLACK); }
+
+  constexpr bool in_bounds(std::size_t i) const {
+    return (i >= 0 && i < WIDTH * HEIGHT);
+  }
+};
