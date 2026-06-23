3e10be135f928931f4ac31670e73821d10d50ee5
Abhay Manoj <sanicthecheese@gmail.com>
2026-06-23 17:53:00 -0400

    feat: added config with title, width, height and aspect ratio

---
+++ include/config.hpp
@@ -0,0 +1,5 @@
+inline constexpr char TITLE[]{"Abhay's Path Tracer"};
+
+inline constexpr auto WIDTH{1280};
+inline constexpr auto HEIGHT{720};
+inline constexpr auto ASPECT{static_cast<double>(WIDTH) / HEIGHT};
