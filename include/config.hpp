83fc5dd3672d54ed1c614b15070566fec42bf2e7
Abhay Manoj <sanicthecheese@gmail.com>
2026-06-24 17:01:00 -0400

    feat: added interval struct and infinity constant to config.hpp

--- include/config.hpp
+++ include/config.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <limits>
+
 inline constexpr char TITLE[]{"Abhay's Path Tracer"};
 
 inline constexpr auto WIDTH{1280};
@@ -8,3 +10,5 @@ inline constexpr auto ASPECT{static_cast<float>(WIDTH) / HEIGHT};
 
 inline constexpr auto FOV{90.0f};
 inline constexpr auto SAMPLE_COUNT{10};
+
+inline constexpr auto INFINITY{std::numeric_limits<float>::infinity()};
---
+++ include/interval.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+
+struct Interval {
+  float min;
+  float max;
+
+  constexpr Interval() : min(INFINITY), max(-INFINITY) {}
+
+  constexpr Interval(float min, float max) : min(min), max(max) {}
+
+  constexpr Interval(const Interval &a, const Interval &b) {
+    min = std::min(a.min, b.min);
+    max = std::max(a.max, b.max);
+  }
+
+  constexpr float size() const { return max - min; }
+
+  constexpr bool surronds(float x) const { return min < x && x < max; }
+
+  constexpr bool contains(float x) const { return min <= x && x <= max; }
+
+  constexpr float clamp(float x) const { return std::clamp(x, min, max); }
+
+  constexpr Interval expand(float delta) const {
+    float padding{delta / 2};
+    return {min - padding, max + padding};
+  }
+
+  static constexpr Interval empty() { return {INFINITY, -INFINITY}; }
+
+  static constexpr Interval universe() { return {-INFINITY, INFINITY}; }
+};
