8f530fcbd11400f63f485bf04ea84bb923f0d9d5
Abhay Manoj <sanicthecheese@gmail.com>
2026-06-23 21:39:56 -0400

    fix: util class uses floats

--- include/util.hpp
+++ include/util.hpp
@@ -2,14 +2,14 @@
 
 #include <random>
 
-constexpr double deg_to_rad(double d) { return d * std::numbers::pi / 180.0; }
+constexpr float deg_to_rad(float d) { return d * std::numbers::pi / 180.0f; }
 
-inline double random_double() {
+inline float random_float() {
   static std::uniform_real_distribution<double> distribution(0.0, 1.0);
   static std::mt19937 generator;
   return distribution(generator);
 }
 
-inline double random_double(double min, double max) {
-  return min + (max - min) * random_double();
+inline float random_float(float min, float max) {
+  return min + (max - min) * random_float();
 }
