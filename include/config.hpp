efbcba5205e0a0541be85801de1ef4aed6406ad2
Abhay Manoj <melchior88729@gmail.com>
2026-06-26 16:46:26 -0400

    chore: shrank constants for perf

--- include/config.hpp
+++ include/config.hpp
@@ -4,13 +4,13 @@
 
 inline constexpr char TITLE[]{"Abhay's Path Tracer"};
 
-inline constexpr auto WIDTH{1280};
-inline constexpr auto HEIGHT{720};
+inline constexpr auto WIDTH{640};
+inline constexpr auto HEIGHT{360};
 inline constexpr auto ASPECT{static_cast<float>(WIDTH) / HEIGHT};
 
 inline constexpr auto FOV{90.0f};
 inline constexpr auto SAMPLE_COUNT{10};
 
-inline constexpr auto MAX_DEPTH{50};
+inline constexpr auto MAX_DEPTH{5};
 
 inline constexpr auto INFINITY{std::numeric_limits<float>::infinity()};
