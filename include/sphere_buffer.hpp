fa8e7c24c9ac1820083c900d6efde66aed6b231e
Abhay Manoj <sanicthecheese@gmail.com>
2026-06-27 20:33:38 -0400

    feat: added material index push back in spherebuffer

--- include/sphere_buffer.hpp
+++ include/sphere_buffer.hpp
@@ -9,12 +9,14 @@ struct SphereBuffer {
   std::vector<float> center_y;
   std::vector<float> center_z;
   std::vector<float> radii;
+  std::vector<size_t> materials;
 
-  void add(float x, float y, float z, float r) {
+  void add(float x, float y, float z, float r, size_t mat_i) {
     center_x.push_back(x);
     center_y.push_back(y);
     center_z.push_back(z);
     radii.push_back(r);
+    materials.push_back(mat_i);
   }
 };
 
@@ -53,7 +55,7 @@ static bool hit_sphere(const SphereBuffer &spheres, const Ray &ray, size_t i,
   record.p = ray.at(root);
   auto surface_normal{(record.p - center) / r};
   record.set_normal(ray, surface_normal);
-  // record.material = material;
+  record.mat_idx = spheres.materials[i];
   return true;
 }
 
