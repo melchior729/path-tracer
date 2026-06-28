b4defdd82ad4e8f5773ae7ddefbded7c07d74980
Abhay Manoj <sanicthecheese@gmail.com>
2026-06-27 20:34:11 -0400

    feat: added per material behaviour + gamma correction

--- include/render.hpp
+++ include/render.hpp
@@ -3,30 +3,54 @@
 #include "camera.hpp"
 #include "color.hpp"
 #include "config.hpp"
+#include "constants.hpp"
 #include "frame_buffer.hpp"
 #include "hit_record.hpp"
+#include "material.hpp"
 #include "sphere_buffer.hpp"
 #include "util.hpp"
 #include "vec3.hpp"
 #include <cmath>
 
-static constexpr Vec3 WHITE{1.0f, 1.0f, 1.0f};
-static constexpr Vec3 SKY_BLUE{0.53f, 0.81f, 0.92f};
-
 static Vec3 sample_square() {
   return {random_float() - 0.5f, random_float() - 0.5f, 0};
 }
 
-static constexpr Vec3 ray_color(const Ray &ray, const SphereBuffer &spheres,
-                                int depth) {
+static Vec3 ray_color(const Ray &ray, const SphereBuffer &spheres,
+                      const std::vector<Material> &materials, int depth) {
   if (depth <= 0) {
     return {};
   }
 
   HitRecord record;
   if (hit_spheres(spheres, ray, Interval{0.001, INFINITY}, record)) {
-    auto dir{record.normal + random_norm()};
-    return 0.5 * ray_color({record.p, dir}, spheres, depth - 1);
+    Ray scattered;
+    Vec3 attenuation;
+
+    auto material{materials[record.mat_idx]};
+    switch (material.type) {
+    case MaterialType::Lambertian:
+      if (material.lambertian.scatter(record, attenuation, scattered)) {
+        return attenuation *
+               ray_color(scattered, spheres, materials, depth - 1);
+      }
+      break;
+
+    case MaterialType::Metal:
+      if (material.metal.scatter(ray, record, attenuation, scattered)) {
+        return attenuation *
+               ray_color(scattered, spheres, materials, depth - 1);
+      }
+      break;
+
+    case MaterialType::Dielectric:
+      if (material.dielectric.scatter(ray, record, attenuation, scattered)) {
+        return attenuation *
+               ray_color(scattered, spheres, materials, depth - 1);
+      }
+
+      break;
+    }
   }
 
   auto dir{norm(ray.direction())};
@@ -34,18 +58,30 @@ static constexpr Vec3 ray_color(const Ray &ray, const SphereBuffer &spheres,
   return (1.0f - a) * WHITE + a * SKY_BLUE;
 }
 
+static float gamma(float v) { return v > 0 ? std::sqrt(v) : 0; }
+
+static Vec3 gamma_vec(const Vec3 &v) {
+  static constexpr Interval intensity{0.000, 0.999};
+  auto r_byte{intensity.clamp(gamma(v.x()))};
+  auto g_byte{intensity.clamp(gamma(v.y()))};
+  auto b_byte{intensity.clamp(gamma(v.z()))};
+  return {r_byte, g_byte, b_byte};
+}
+
 inline void render(const Camera &camera, const SphereBuffer &spheres,
+                   const std::vector<Material> &materials,
                    FrameBuffer &buffer) {
   for (size_t j{}; j < HEIGHT; ++j) {
     for (size_t i{}; i < WIDTH; ++i) {
       Vec3 col;
       for (size_t s{}; s < SAMPLE_COUNT; ++s) {
         auto ray{camera.get_ray(sample_square(), i, j)};
-        col += ray_color(ray, spheres, MAX_DEPTH);
+        col += ray_color(ray, spheres, materials, MAX_DEPTH);
       }
 
       col /= SAMPLE_COUNT;
-      buffer.set(i, j, Color{col});
+      auto writtable{gamma_vec(col)};
+      buffer.set(i, j, Color{writtable});
     }
   }
 }
