0733d3a1cc1f0cc49b9c4a7658ede533073689ef
Abhay Manoj <sanicthecheese@gmail.com>
2026-06-27 20:32:53 -0400

    chore: updated constants for testing

--- include/config.hpp
+++ include/config.hpp
@@ -4,13 +4,13 @@
 
 inline constexpr char TITLE[]{"Abhay's Path Tracer"};
 
-inline constexpr auto WIDTH{640};
-inline constexpr auto HEIGHT{360};
+inline constexpr auto WIDTH{320};
+inline constexpr auto HEIGHT{180};
 inline constexpr auto ASPECT{static_cast<float>(WIDTH) / HEIGHT};
 
 inline constexpr auto FOV{90.0f};
-inline constexpr auto SAMPLE_COUNT{10};
+inline constexpr auto SAMPLE_COUNT{20};
 
-inline constexpr auto MAX_DEPTH{5};
+inline constexpr auto MAX_DEPTH{20};
 
 inline constexpr auto INFINITY{std::numeric_limits<float>::infinity()};
