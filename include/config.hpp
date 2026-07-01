0663eb6d9318372c2456bff4bd529f561cc62625
Abhay Manoj <melchior88729@gmail.com>
2026-06-30 20:27:57 -0400

    fix: max_depth now size_t

--- include/config.hpp
+++ include/config.hpp
@@ -11,6 +11,6 @@ inline constexpr auto ASPECT{static_cast<float>(WIDTH) / HEIGHT};
 inline constexpr auto FOV{90.0f};
 inline constexpr auto SAMPLE_COUNT{20};
 
-inline constexpr auto MAX_DEPTH{20};
+inline constexpr std::size_t MAX_DEPTH{20};
 
 inline constexpr auto INFINITY{std::numeric_limits<float>::infinity()};
