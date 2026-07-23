f4db58ae9b826f3516bd92ab533d056bece665f1
Abhay Manoj <melchior88729@gmail.com>
2026-07-23 22:16:00 +0000

    feat: hot-swap scenes from keyboard
    Co-authored-by: Abhay Manoj <melchior88729@gmail.com>

--- include/gpu/gpu_memory.hpp
+++ include/gpu/gpu_memory.hpp
@@ -4,6 +4,10 @@
 
 void init_gpu_state(GPUState *gpu, const CPUState *cpu);
 
+void upload_scene_to_gpu(GPUState *gpu, const CPUState *cpu);
+
+void destroy_gpu_state(GPUState *gpu);
+
 void init_gpu_rng(void **state, size_t seed);
 
 void copy_cam_to_gpu(Camera *d_c, const Camera *c);
--- src/gpu_memory.cu
+++ src/gpu_memory.cu
@@ -27,32 +27,63 @@ template <typename T> static void copy_to_gpu(T **dest, T *src, size_t N) {
   CUDA_CHECK(cudaMemcpy(*dest, src, T_bytes_per_arr, cudaMemcpyHostToDevice));
 }
 
-void init_gpu_state(GPUState *gpu, const CPUState *cpu) {
-  // CUDA_CHECK(cudaDeviceSetLimit(cudaLimitStackSize, 4096););
+template <typename T> static void free_gpu_ptr(T *&ptr) {
+  if (ptr != nullptr) {
+    CUDA_CHECK(cudaFree(ptr));
+    ptr = nullptr;
+  }
+}
 
-  float *d_x;
-  float *d_y;
-  float *d_z;
-  float *d_r;
-  size_t *d_m;
-  BVHNode *d_root;
+static void destroy_gpu_scene(GPUState *gpu) {
+  free_gpu_ptr(gpu->sphere_x);
+  free_gpu_ptr(gpu->sphere_y);
+  free_gpu_ptr(gpu->sphere_z);
+  free_gpu_ptr(gpu->sphere_radii);
+  free_gpu_ptr(gpu->sphere_materials);
+  free_gpu_ptr(gpu->bvh_nodes);
+  free_gpu_ptr(gpu->materials);
+  gpu->mat_size = 0;
+}
+
+void upload_scene_to_gpu(GPUState *gpu, const CPUState *cpu) {
+  destroy_gpu_scene(gpu);
 
   auto size{cpu->spheres->size};
-  copy_to_gpu(&d_x, cpu->spheres->center_x, size);
-  copy_to_gpu(&d_y, cpu->spheres->center_y, size);
-  copy_to_gpu(&d_z, cpu->spheres->center_z, size);
-  copy_to_gpu(&d_r, cpu->spheres->radii, size);
-  copy_to_gpu(&d_m, cpu->spheres->materials, size);
+  copy_to_gpu(&gpu->sphere_x, cpu->spheres->center_x, size);
+  copy_to_gpu(&gpu->sphere_y, cpu->spheres->center_y, size);
+  copy_to_gpu(&gpu->sphere_z, cpu->spheres->center_z, size);
+  copy_to_gpu(&gpu->sphere_radii, cpu->spheres->radii, size);
+  copy_to_gpu(&gpu->sphere_materials, cpu->spheres->materials, size);
   copy_to_gpu(&gpu->materials, cpu->materials->data(), cpu->mat_size);
-  copy_to_gpu(&d_root, cpu->tree->root, 2 * size - 1);
+  copy_to_gpu(&gpu->bvh_nodes, cpu->tree->root, 2 * size - 1);
+
+  init_gpu_sphere_buffer<<<1, 1>>>(
+      gpu->spheres, gpu->sphere_x, gpu->sphere_y, gpu->sphere_z,
+      gpu->sphere_radii, gpu->sphere_materials, size);
+  init_bvh_tree<<<1, 1>>>(gpu->tree, gpu->bvh_nodes, size);
+  CUDA_CHECK(cudaGetLastError());
+  CUDA_CHECK(cudaDeviceSynchronize());
+  gpu->mat_size = cpu->mat_size;
+}
+
+void init_gpu_state(GPUState *gpu, const CPUState *cpu) {
+  // CUDA_CHECK(cudaDeviceSetLimit(cudaLimitStackSize, 4096););
 
   CUDA_CHECK(cudaMalloc((void **)&gpu->spheres, sizeof(SphereBuffer)));
   CUDA_CHECK(cudaMalloc((void **)&gpu->camera, sizeof(Camera)));
   CUDA_CHECK(cudaMalloc((void **)&gpu->buffer, sizeof(FrameBuffer)));
   CUDA_CHECK(cudaMalloc((void **)&gpu->tree, sizeof(BVHTree)));
 
-  init_gpu_sphere_buffer<<<1, 1>>>(gpu->spheres, d_x, d_y, d_z, d_r, d_m, size);
-  init_bvh_tree<<<1, 1>>>(gpu->tree, d_root, 2 * size - 1);
+  upload_scene_to_gpu(gpu, cpu);
+}
+
+void destroy_gpu_state(GPUState *gpu) {
+  destroy_gpu_scene(gpu);
+  free_gpu_ptr(gpu->spheres);
+  free_gpu_ptr(gpu->camera);
+  free_gpu_ptr(gpu->buffer);
+  free_gpu_ptr(gpu->tree);
+  free_gpu_ptr(gpu->generator);
 }
 
 void move_fb_to_cpu(FrameBuffer *b, const FrameBuffer *d_b) {
